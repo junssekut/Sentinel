221c82eb6569edeb18f124a98673913d102b27fc
F2LX <felixfebryan16@gmail.com>
2025-12-27 14:34:09 +0700

    fix: OpenMP DLL clashes on windows (client)

--- client/main.py
+++ client/main.py
@@ -1,10 +1,9 @@
-import tkinter as tk
-import os
-from src.ui.main_window import FaceClientApp
-
 # Mitigate OpenMP DLL clashes on Windows
+import os
 os.environ.setdefault("KMP_DUPLICATE_LIB_OK", "TRUE")
 os.environ.setdefault("OMP_NUM_THREADS", "1")
+import tkinter as tk
+from src.ui.main_window import FaceClientApp
 
 def main():
     root = tk.Tk()
